511c91414842c2715f2bdde094595bda7c0b0b33
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-02-10 19:53:24 +0700

    feat(data/users.ts): create dummy data

---
+++ data/users.ts
@@ -0,0 +1,27 @@
+export default [
+  {
+    fullName: "Angelina Runolfsson",
+    email: "Elnora19@hotmail.com",
+    id: "1",
+  },
+  {
+    fullName: "Angelina Runolfsson",
+    email: "Elnora19@hotmail.com",
+    id: "2",
+  },
+  {
+    fullName: "Mildred Rempel",
+    email: "Maudie33@hotmail.com",
+    id: "3",
+  },
+  {
+    fullName: "Edith Emard",
+    email: "Garry_Daniel44@hotmail.com",
+    id: "4",
+  },
+  {
+    fullName: "Lionel Herman",
+    email: "Leda_Sporer85@yahoo.com",
+    id: "5",
+  },
+];
